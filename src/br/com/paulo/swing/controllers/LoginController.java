2174cb3b6f8c1006dc4e5047d6896d19dd7aca5f
Paulo Gabriel <pgj1085@gmail.com>
2020-06-29 20:46:44 -0300

    Tela de login

---
+++ src/br/com/paulo/swing/controllers/LoginController.java
@@ -0,0 +1,23 @@
+package br.com.paulo.swing.controllers;
+
+import br.com.paulo.swing.db.LoginDao;
+import br.com.paulo.swing.models.Login;
+
+public class LoginController {
+    
+    private LoginDao loginDao;
+    
+    public LoginController(LoginDao loginDao){
+        this.loginDao = loginDao;
+    }
+
+    public void setLoginDao(LoginDao loginDao) {
+        this.loginDao = loginDao;
+    }
+    
+    
+    public Login getLoginn(String login, String senha){
+        return loginDao.getLogin(login, senha);
+    }
+
+}
